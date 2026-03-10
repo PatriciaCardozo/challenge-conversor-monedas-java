33cd7b77de1e368528bdd14a91c1b4ad271aa0df
Patricia <patriciabader39@gmail.com>
2026-03-10 12:04:40 +0100

    Actualizar Conversor.java para usar la API real

--- src/src/Conversor.java
+++ src/src/Conversor.java
@@ -1,50 +1,56 @@
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+
 public class Conversor {
 
-    public void convertir(int opcion, double cantidad){
+    private static final String API_KEY = "255e7bc526e4eeed2e6d5ae0";
 
-        double tasa = 0;
+    public void convertir(int opcion, double cantidad) {
+
+        String url = "";
         String moneda = "";
 
-        switch(opcion){
-
+        switch (opcion) {
             case 1:
-                tasa = 850;
+                url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/USD/ARS";
                 moneda = "ARS";
                 break;
-
             case 2:
-                tasa = 0.0012;
+                url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/ARS/USD";
                 moneda = "USD";
                 break;
-
             case 3:
-                tasa = 5.0;
+                url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/USD/BRL";
                 moneda = "BRL";
                 break;
-
             case 4:
-                tasa = 0.20;
+                url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/BRL/USD";
                 moneda = "USD";
                 break;
-
             case 5:
-                tasa = 4000;
+                url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/USD/COP";
                 moneda = "COP";
                 break;
-
             case 6:
-                tasa = 0.00025;
+                url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/COP/USD";
                 moneda = "USD";
                 break;
-
             default:
                 System.out.println("Opción inválida");
                 return;
         }
 
-        double resultado = cantidad * tasa;
+        try {
+            String respuesta = ApiClient.obtenerDatos(url);
+            JsonObject json = JsonParser.parseString(respuesta).getAsJsonObject();
+            double tasa = json.get("conversion_rate").getAsDouble();
 
-        System.out.println("El valor convertido es: " + resultado + " " + moneda);
+            double resultado = cantidad * tasa;
+            System.out.println("El valor convertido es: " + resultado + " " + moneda);
 
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Error al obtener la tasa de cambio: " + e.getMessage());
+        }
     }
 }
